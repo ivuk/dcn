6c29447006b34ffd8206c936344f437bfd64e9fa
Igor Vuk <parcijala@gmail.com>
2011-04-20 23:27:32 +0200

    Added _id generation.

--- dcn.py
+++ dcn.py
@@ -3,6 +3,7 @@ from desktopcouch.records.server import CouchDatabase
 from desktopcouch.records.record import Record
 import os
 import sys
+from uuid import uuid4
 
 dbname = 'dcn_'+str(os.getuid())
 try:
@@ -13,51 +14,49 @@ except Exception:
 	print 'Database created.'''
 
 def func(param, param2):
-	record_type = 'http://example.com/fetch-record-type.html'
-	if param == '-i':
-		if param2 != '':
-			new_row = Record({'row':str(param2)}, record_type)
-			try:
-				db.put_record(new_row)
-			except Exception:
-				raise
-			else:
-				print 'New row created.'
-		else:
-			row = raw_input('Insert text here:  ')
-			new_row = Record({'row':str(row)}, record_type)
-			try:
-				db.put_record(new_row)
-			except Exception:
-				raise
-			else:
-				print 'New row created.'
-
-	elif param == '-d':
-		if param2 != '':
-			print 'brisanje'
-		else:
-			func('-l','')
-			dvar = raw_input('Which record do you want to delete: ')
-			print dvar
-
-	elif param == '-l':
-		results = db.get_records(record_type = record_type, create_view = True)
-		for records in results:
-			record = records.value
-			print record
+    record_type = 'http://example.com/fetch-record-type.html'
+    if param == '-i':
+        if param2 != '':
+            new_row = Record({'_id': uuid4().hex,'data':str(param2)}, record_type)
+            try:
+                db.put_record(new_row)
+            except Exception:
+                raise
+            else:
+                print 'New row created.'
+        else:
+            row = raw_input('Insert text here:  ')
+            new_row = Record({'row':str(row)}, record_type)
+            try:
+                db.put_record(new_row)
+            except Exception:
+                raise
+            else:
+                print 'New row created.'
+    elif param == '-d':
+        if param2 != '':
+            print 'brisanje'
+        else:
+            func('-l','')
+            dvar = raw_input('Which record do you want to delete: ')
+            print dvar
+    elif param == '-l':
+        results = db.get_records(record_type = record_type, create_view = True)
+        for records in results:
+            record = records.value
+            print record
 
 class base:
 # base.record_type
     record_type = 'http://example.com/fetch-record-type.html'
 # base.insert
-    def insert(self):
+    def insert(self, param):
         return True
 # base.delete
     def delete(self):
         return True
 # base.list
-    def list(self):
+    def list(self, param):
         return True
 # konstruktor
 def __init__(self):
